4e023cfb527c8e28a0973c41ff0bc6f0a0cee769
Nevin Jojo <nevinjojo98@gmail.com>
2019-09-08 12:52:49 +1200

    Reset Project Structure
    The project is not using Express.js anymore since hosting a web app on Heroku costs $$ and the loading time is bad.
    
    The new version is purse html, css, javascript and a little bit of jquery.

---
+++ js/resources.js
@@ -0,0 +1,309 @@
+let selected_category = "All";
+
+let category_colours = {
+  "All": "#000000",
+  "Photos": "#1BC7A4",
+  "Mockups": "#01B0D6",
+  "Illustration": "#3684EF",
+  "Icons": "#8F55E2",
+  "Fonts": "#E56FBB",
+  "Colour": "#FF3423",
+  "Inspiration": "#FF8300",
+  "UX & UI": "#FFBA01",
+  "Other": "#42C241"
+};
+
+let resources = [
+  {
+    title: "Kaboompics",
+    description: "Great place to get breathtaking free pictures for business or personal projects.",
+    category: "Photos",
+    job_types: ["Designer, Web Designer"],
+    url: "http://kaboompics.com/",
+    icon_link: "images/resource_icons/1_Kaboompics.svg"
+  },
+  {
+    title: "Pexels",
+    description: "Free high quality photos you can use anywhere that require no attribution.",
+    category: "Photos",
+    job_types: ["Designer, Web Designer"],
+    url: "http://pexels.com/",
+    icon_link: "images/resource_icons/1_Pexels.svg"
+  },
+  {
+    title: "Stocksnap",
+    description: "High quality and high resolution images free from all copyright restrictions.",
+    category: "Photos",
+    job_types: ["Designer, Web Designer"],
+    url: "https://stocksnap.io/",
+    icon_link: "images/resource_icons/1_Stocksnap.svg"
+  },
+  {
+    title: "Unsplash",
+    description: "The internet's source of freely usable images. Powered by creators everywhere.",
+    category: "Photos",
+    job_types: ["Designer, Web Designer"],
+    url: "https://unsplash.com/",
+    icon_link: "images/resource_icons/1_Unsplash.svg"
+  },
+  {
+    title: "Graphic Burger",
+    description: "Tasty design resources made with care that are free for commercial use.",
+    category: "Mockups",
+    job_types: ["Designer, Web Designer"],
+    url: "https://graphicburger.com/",
+    icon_link: "images/resource_icons/2_Graphicburger.svg"
+  },
+  {
+    title: "Pixeden",
+    description: "Premium design mockups for packaging, stationery, technology and more.",
+    category: "Mockups",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.pixeden.com/free-design-web-resources",
+    icon_link: "images/resource_icons/2_Pixaden.svg"
+  },
+  {
+    title: "Pixel Buddha",
+    description: "Free mock-ups and various other design resources such as UI kits & fonts.",
+    category: "Mockups",
+    job_types: ["Designer, Web Designer"],
+    url: "https://pixelbuddha.net/freebies",
+    icon_link: "images/resource_icons/2_Pixelbuddha.svg"
+  },
+  {
+    title: "Humaaans",
+    description: "Free mix & match illustrations of minimal people with a customisable design library.",
+    category: "Illustration",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.humaaans.com",
+    icon_link: "images/resource_icons/3_Humaaans.svg"
+  },
+  {
+    title: "Ouch!",
+    description: "Free beautiful illustration sets by various different designers. Tons of styles to choose.",
+    category: "Illustration",
+    job_types: ["Designer, Web Designer"],
+    url: "https://icons8.com/ouch",
+    icon_link: "images/resource_icons/3_Ouch.svg"
+  },
+  {
+    title: "The Noun Project",
+    description: "Over 2 million curated icons in various styles and packs.",
+    category: "Icons",
+    job_types: ["Designer, Web Designer"],
+    url: "https://thenounproject.com",
+    icon_link: "images/resource_icons/4_Thenounproject.svg"
+  },
+  {
+    title: "Flat Icon",
+    description: "Curated packs of icons that are great for keeping a consistent style throughout a project.",
+    category: "Icons",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.flaticon.com",
+    icon_link: "images/resource_icons/4_Flaticon.svg"
+  },
+  {
+    title: "Icons8",
+    description: "Great minimal icon packs for UI projects that you can filter by device type.",
+    category: "Icons",
+    job_types: ["Designer, Web Designer"],
+    url: "https://icons8.com/icons",
+    icon_link: "images/resource_icons/4_Icons8.svg"
+  },
+  {
+    title: "Icon Finder",
+    description: "Various different icon styles including coloured and isometric sets.",
+    category: "Icons",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.iconfinder.com/icon-sets/featured/free",
+    icon_link: "images/resource_icons/4_Iconfinder.svg"
+  },
+  {
+    title: "Google Fonts",
+    description: "The best website for implementing fonts into online projects, or quickly downloading great simple fonts.",
+    category: "Fonts",
+    job_types: ["Designer, Web Designer"],
+    url: "https://fonts.google.com",
+    icon_link: "images/resource_icons/5_Googlefonts.svg"
+  },
+  {
+    title: "Calligraphr",
+    description: "Easy way to transform your handwriting into its own font to share or use in projects.",
+    category: "Fonts",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.calligraphr.com/en/",
+    icon_link: "images/resource_icons/5_Calligraphr.svg"
+  },
+  {
+    title: "Font Squirrel",
+    description: "Great collections of free fonts for commercial use with great filters.",
+    category: "Fonts",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.fontsquirrel.com",
+    icon_link: "images/resource_icons/5_Fontsquirrel.svg"
+  },
+  {
+    title: "Font Space",
+    description: "Collection of more obscure and unusual fonts that are great for design projects.",
+    category: "Fonts",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.fontspace.com",
+    icon_link: "images/resource_icons/5_Fontspace.svg"
+  },
+  {
+    title: "Coolors",
+    description: "Best colour palette generator website with public gallery to find or download great inspiration.",
+    category: "Colour",
+    job_types: ["Designer, Web Designer"],
+    url: "https://coolors.co/ea526f-e76b74-d7af70-937d64-585b56",
+    icon_link: "images/resource_icons/6_Coolors.svg"
+  },
+  {
+    title: "Colour Hunt",
+    description: "Curated list of soft and beautiful colour palettes.",
+    category: "Colour",
+    job_types: ["Designer, Web Designer"],
+    url: "https://colorhunt.co",
+    icon_link: "images/resource_icons/6_Colourhunt.svg"
+  },
+  {
+    title: "Gradients.io",
+    description: "Short list of colourful and bold gradient for modern design projects.",
+    category: "Colour",
+    job_types: ["Designer, Web Designer"],
+    url: "http://gradients.io",
+    icon_link: "images/resource_icons/6_Gradientsio.svg"
+  },
+  {
+    title: "Khroma",
+    description: "An AI colour tool that learns off you to recommend you personalised colour palettes.",
+    category: "Colour",
+    job_types: ["Designer, Web Designer"],
+    url: "http://khroma.co/generator/",
+    icon_link: "images/resource_icons/6_Khroma.svg"
+  },
+  {
+    title: "Awwwards",
+    description: "Best curated list and collects of interesting and trending website inspiration.",
+    category: "Inspiration",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.awwwards.com",
+    icon_link: "images/resource_icons/7_Awwwards.svg"
+  },
+  {
+    title: "Behance",
+    description: "Adobe's website for creatives to show off great in depth design projects.",
+    category: "Inspiration",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.behance.net",
+    icon_link: "images/resource_icons/7_Behance.svg"
+  },
+  {
+    title: "Dribbble",
+    description: "Quick snippets of colourful and exciting UI and illustration inspiration.",
+    category: "Inspiration",
+    job_types: ["Designer, Web Designer"],
+    url: "https://dribbble.com",
+    icon_link: "images/resource_icons/7_Dribbble.svg"
+  },
+  {
+    title: "Mobbin",
+    description: "Great collection of the latest iOS UI design patterns and screens.",
+    category: "UX & UI",
+    job_types: ["Designer, Web Designer"],
+    url: "https://mobbin.design",
+    icon_link: "images/resource_icons/8_Mobbin.svg"
+  },
+  {
+    title: "Sketchsheets",
+    description: "Ready to print sketch sheet templates for UX designers.",
+    category: "UX & UI",
+    job_types: ["Designer, Web Designer"],
+    url: "https://sketchsheets.com",
+    icon_link: "images/resource_icons/8_Sketchsheets.svg"
+  },
+  {
+    title: "UI Sources",
+    description: "More creative and unusual UI modern design patterns and screens.",
+    category: "UX & UI",
+    job_types: ["Designer, Web Designer"],
+    url: "https://www.uisources.com",
+    icon_link: "images/resource_icons/8_Uisources.svg"
+  },
+  {
+    title: "Paper Sizes",
+    description: "The best resource for international paper sizes, dimensions & formats.",
+    category: "Other",
+    job_types: ["Designer, Web Designer"],
+    url: "https://papersizes.io",
+    icon_link: "images/resource_icons/9_Papersizes.svg"
+  },
+  {
+    title: "Briefbox",
+    description: "Practice design briefs & skill development for when you feel stuck.",
+    category: "Other",
+    job_types: ["Designer, Web Designer"],
+    url: "https://briefbox.me",
+    icon_link: "images/resource_icons/9_Briefbox.svg"
+  },
+  {
+    title: "SmallPDF",
+    description: "All-in-one easy-to-use online PDF tools with tons of different quick features.",
+    category: "Other",
+    job_types: ["Designer, Web Designer"],
+    url: "https://smallpdf.com",
+    icon_link: "images/resource_icons/9_Smallpdf.svg"
+  }
+];
+
+function resetResources() {
+  let resourceDiv = document.getElementById('resourceRow');
+  let child = resourceDiv.lastElementChild;
+  while (child) {
+    resourceDiv.removeChild(child);
+    child = resourceDiv.lastElementChild;
+  }
+}
+
+function loadResources(category) {
+  resetResources();
+  selected_category = category;
+  document.getElementById("dropdownMenu3").innerHTML = category;
+  let selected_resources = [];
+  for (let resource of resources) {
+    if (resource.category === selected_category || selected_category === "All") {
+      selected_resources.push(resource);
+    }
+  }
+  createResources(selected_resources);
+}
+
+function createResources(resources) {
+  for (let resource of resources) {
+    let resourceString = '<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 resource-card">' +
+      '        <a href="' + resource.url + '" rel="noopener" target="_blank">' +
+      '          <div class="resource-card-wrapper">' +
+      '            <div class="card-content">' +
+      '              <div class="card-open">' +
+      '                <img src="images/open.svg">' +
+      '              </div>' +
+      '              <div class="card-icon-container">' +
+      '                <div class="card-icon" alt="' + resource.title + 'Logo">' +
+      '                  <img src="' + resource.icon_link + '" alt="' + resource.title + 'Logo"/>' +
+      '                </div>' +
+      '              </div>' +
+      '              <div class="card-text-container">' +
+      '                <div class="card-text card-category"' +
+      '                     style="color: ' + category_colours[resource.category] + '">' + resource.category + '</div>' +
+      '                <div class="card-text card-title">' + resource.title + '</div>' +
+      '                <div class="card-text card-description">' + resource.description + '</div>' +
+      '              </div>' +
+      '            </div>' +
+      '          </div>' +
+      '        </a>' +
+      '      </div>'
+    ;
+    let div = document.getElementById('resourceRow');
+    div.insertAdjacentHTML('beforeend', resourceString);
+  }
+}
