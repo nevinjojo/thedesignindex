fb82f90706498f2e98ea37f4cea1d5d150a887df
Nevin Jojo <nevinjojo98@gmail.com>
2019-09-15 15:03:48 +1200

    Update form in feedback page

--- feedback.php
+++ feedback.php
@@ -1,4 +1,21 @@
-<!doctype html>
+<?php
+
+if($_POST["submit"]) {
+    $recipient="nevinjojo98@gmail.com";
+    $subject="Feedback: The Design Index";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you for your feedback! Your message has been sent.</p>";
+}
+
+?><!DOCTYPE html>
+
 <html lang="en" xml:lang="en">
 <head>
   <title>The Design Index</title>
@@ -50,7 +67,7 @@
   <meta name="theme-color" content="#ffffff">
 </head>
 
-<body onload="loadResources(selected_category)">
+<body>
 <!-- Header -->
 <nav class="navbar">
   <!-- Top Header -->
@@ -69,7 +86,20 @@
 <!-- Resource Wrapper -->
 <div class="box">
   <div class="resource-wrapper">
+    <?=$thankYou ?>
 
+    <form method="post" action="contact.php">
+      <label>Name:</label>
+      <input name="sender">
+
+      <label>Email address:</label>
+      <input name="senderEmail">
+
+      <label>Message:</label>
+      <textarea rows="5" cols="20" name="message"></textarea>
+
+      <input type="submit" name="submit">
+    </form>
   </div>
 </div>
 
@@ -87,4 +117,17 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+</html>
+
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Contact form to email</title>
+</head>
+
+<body>
+
+</body>
+
 </html>
