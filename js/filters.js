cdcd0d8be76b1ce76383bc43c935fe537a78b2cf
NikolaiBain <NikolaiBain@github.com>
2019-12-01 13:25:37 +1300

    hidden filters on mobile

---
+++ js/filters.js
@@ -0,0 +1,10 @@
+// Show/hide filters
+
+function showFilters() {
+  var x = document.getElementById("dropdowns");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
